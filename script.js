39b1ba92a6170760488cd7500c8ed645ec67297f
petya <petya52@gmail.com>
2025-12-07 15:38:10 +0200

    push

---
+++ script.js
@@ -0,0 +1,39 @@
+const API_URL = "https://my-json-server.typicode.com/blacksempai/marketplace/db"
+
+async function getProductsDb () {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+
+    return data.products;
+}
+getProductsDb().then(data => console.log(data))
+
+const main = document.getElementById("main");
+
+async function renderProductsCard () {
+    const products = await getProductsDb();
+
+    products.forEach(product => {
+        const card = document.createElement("div");
+        card.className = "product-card";
+
+        card.innerHTML = `
+            <h3 class="name">${product.name}</h3>
+            <img class="image" src="${product.photo_url}">
+            <p class="price">
+                <span class="sub-item">Price:</span> 
+                ${product.price}
+            </p>
+            <p class="description">
+            <spand class="sub-item">Description:</span> 
+            ${product.description}
+            </p>
+
+            <a class="seller-profile-link" href="#">Seller Profile</a>
+            <button class="buy-button">Buy</button>
+        `;
+        
+        main.append(card)
+    });
+}
+renderProductsCard()
